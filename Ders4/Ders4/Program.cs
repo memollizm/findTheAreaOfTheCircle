066ee8e603331cb1f3abb4c086ae2260a836ede9
Mehmet Başgöze <92752077+memollizm@users.noreply.github.com>
2022-12-11 16:43:23 +0300

    Add files via upload

---
+++ Ders4/Ders4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaAndPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area,perimeter;
+
+            Console.Write("Input the radius: ");
+            radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("****************************");
+            
+            perimeter = 2 * Math.PI * radius;
+            area = Math.PI * radius * radius;
+
+            Console.WriteLine("Area = "+area);
+            Console.WriteLine("Perimeter = "+perimeter);
+            Console.ReadKey();
+        }
+    }
+}
